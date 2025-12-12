97b77bd4634d650ace5e7b2a943f401b18fa5c8f
yonghao.fyh <yonghao.fyh@alibaba-inc.com>
2025-12-12 10:22:41 +0000

    chore: add test for workflows

---
+++ ci/scripts/build_paimon.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+# Copyright 2024-present Alibaba Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eux
+
+source_dir=${1}
+build_dir=${1}/build
+
+mkdir ${build_dir}
+pushd ${build_dir}
+
+CMAKE_ARGS=(
+    "-G Ninja"
+    "-DCMAKE_BUILD_TYPE=Debug"
+    "-DPAIMON_BUILD_TESTS=ON"
+    "-DPAIMON_ENABLE_LANCE=ON"
+    "-DPAIMON_ENABLE_JINDO=ON"
+    "-DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX:-${PAIMON_HOME}}"
+)
+
+cmake "${CMAKE_ARGS[@]}" ${source_dir}
+cmake --build . --target install -- -j$(nproc)
+ctest --output-on-failure
+
+popd
+
+rm -rf ${build_dir}
