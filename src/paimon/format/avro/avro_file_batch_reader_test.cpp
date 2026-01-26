1697e7e190eaf9dc718b62abf85b692a29dcc59b
jinli.zjw <jinli.zjw@alibaba-inc.com>
2026-01-26 11:30:19 +0800

    fix

--- src/paimon/format/avro/avro_file_batch_reader_test.cpp
+++ src/paimon/format/avro/avro_file_batch_reader_test.cpp
@@ -246,6 +246,104 @@ TEST_P(AvroFileBatchReaderTest, TestReadTimestampTypes) {
     ASSERT_TRUE(expected_array->Equals(result_array));
 }
 
+TEST_P(AvroFileBatchReaderTest, TestReadMapTypes) {
+    std::string path = paimon::test::GetDataDir() +
+                       "/avro/append_with_multiple_map.db/"
+                       "append_with_multiple_map/bucket-0/"
+                       "data-72442742-e49e-48a4-a736-a2475aac2d2c-0.avro";
+
+    ASSERT_OK_AND_ASSIGN(auto reader_builder,
+                         file_format_->CreateReaderBuilder(/*batch_size=*/1024));
+    ASSERT_OK_AND_ASSIGN(std::shared_ptr<InputStream> in, fs_->Open(path));
+    ASSERT_OK_AND_ASSIGN(auto batch_reader, reader_builder->Build(in));
+
+    arrow::FieldVector read_fields = {
+        arrow::field("f0", arrow::map(arrow::int32(), arrow::int32())),
+        arrow::field("f1", arrow::map(arrow::float64(), arrow::float64())),
+        arrow::field("f2", arrow::map(arrow::utf8(), arrow::utf8())),
+        arrow::field("f3", arrow::map(arrow::utf8(), arrow::binary())),
+        arrow::field("f4", arrow::map(arrow::timestamp(arrow::TimeUnit::MICRO),
+                                      arrow::timestamp(arrow::TimeUnit::MICRO))),
+        arrow::field("f5", arrow::map(arrow::utf8(), arrow::list(arrow::float64()))),
+        arrow::field("f6", arrow::map(arrow::utf8(), arrow::map(arrow::float64(), arrow::utf8()))),
+        arrow::field("f7", arrow::map(arrow::int64(),
+                                      arrow::struct_({field("f0", arrow::int32()),
+                                                      field("f1", arrow::utf8()),
+                                                      field("f2", arrow::decimal128(5, 2))})))};
+    auto read_schema = arrow::schema(read_fields);
+    std::unique_ptr<ArrowSchema> c_schema = std::make_unique<ArrowSchema>();
+    ASSERT_TRUE(arrow::ExportSchema(*read_schema, c_schema.get()).ok());
+    EXPECT_OK(batch_reader->SetReadSchema(c_schema.get(), /*predicate=*/nullptr,
+                                          /*selection_bitmap=*/std::nullopt));
+
+    // check array
+    ASSERT_OK_AND_ASSIGN(auto result_array,
+                         ::paimon::test::ReadResultCollector::CollectResult(batch_reader.get()));
+    std::shared_ptr<arrow::ChunkedArray> expected_array;
+    auto array_status =
+        arrow::ipc::internal::json::ChunkedArrayFromJSON(arrow::struct_(read_fields), {R"([
+        [
+            [[1,10],[2,20]],
+            [[1.1,10.1],[2.2,20.2]],
+            [["key1","val1"],["key2","val2"]],
+            [["123456","abcdef"]],
+            [["2023-01-01 12:00:00.123000","2023-01-01 12:00:00.123000"],["2023-01-02 13:30:00.456000","2023-01-02 13:30:00.456000"]],
+            [["arr_key",[1.5, 2.5, 3.5]]],
+            [["outer_key",[[99.9,"nested_val"]]]],
+            [[1000, [42, "row_str", "123.45"]]]
+        ]
+    ])"},
+                                                         &expected_array);
+    ASSERT_TRUE(array_status.ok()) << array_status.ToString();
+    ASSERT_TRUE(result_array->Equals(expected_array)) << result_array->ToString() << std::endl;
+    ASSERT_TRUE(expected_array->Equals(result_array));
+}
+
+TEST_P(AvroFileBatchReaderTest, TestReadRowNumbers) {
+    std::string path = paimon::test::GetDataDir() +
+                       "/avro/append_simple.db/"
+                       "append_simple/bucket-0/"
+                       "data-d7d1c416-6e34-4834-af87-341d09418f0c-0.avro";
+
+    ASSERT_OK_AND_ASSIGN(auto reader_builder, file_format_->CreateReaderBuilder(/*batch_size=*/1));
+    ASSERT_OK_AND_ASSIGN(std::shared_ptr<InputStream> in, fs_->Open(path));
+    ASSERT_OK_AND_ASSIGN(auto reader, reader_builder->Build(in));
+
+    arrow::FieldVector read_fields = {
+        arrow::field("f0", arrow::int32()), arrow::field("f1", arrow::float64()),
+        arrow::field("f2", arrow::utf8()),
+        arrow::field("f3",
+                     arrow::struct_({arrow::field("f0", arrow::map(arrow::utf8(), arrow::int32())),
+                                     arrow::field("f1", arrow::list(arrow::int32()))}))};
+
+    auto read_schema = arrow::schema(read_fields);
+    std::unique_ptr<ArrowSchema> c_schema = std::make_unique<ArrowSchema>();
+    ASSERT_TRUE(arrow::ExportSchema(*read_schema, c_schema.get()).ok());
+    EXPECT_OK(reader->SetReadSchema(c_schema.get(), /*predicate=*/nullptr,
+                                    /*selection_bitmap=*/std::nullopt));
+
+    ASSERT_EQ(std::numeric_limits<uint64_t>::max(), reader->GetPreviousBatchFirstRowNumber());
+    ASSERT_OK_AND_ASSIGN(auto batch1, reader->NextBatch());
+    ArrowArrayRelease(batch1.first.get());
+    ArrowSchemaRelease(batch1.second.get());
+    ASSERT_EQ(0, reader->GetPreviousBatchFirstRowNumber());
+    ASSERT_OK_AND_ASSIGN(auto batch2, reader->NextBatch());
+    ASSERT_EQ(1, reader->GetPreviousBatchFirstRowNumber());
+    ArrowArrayRelease(batch2.first.get());
+    ArrowSchemaRelease(batch2.second.get());
+    ASSERT_OK_AND_ASSIGN(auto batch3, reader->NextBatch());
+    ASSERT_EQ(2, reader->GetPreviousBatchFirstRowNumber());
+    ArrowArrayRelease(batch3.first.get());
+    ArrowSchemaRelease(batch3.second.get());
+    ASSERT_OK_AND_ASSIGN(auto batch4, reader->NextBatch());
+    ASSERT_EQ(3, reader->GetPreviousBatchFirstRowNumber());
+    ArrowArrayRelease(batch4.first.get());
+    ArrowSchemaRelease(batch4.second.get());
+    ASSERT_OK_AND_ASSIGN(auto batch5, reader->NextBatch());
+    ASSERT_EQ(4, reader->GetPreviousBatchFirstRowNumber());
+    ASSERT_TRUE(BatchReader::IsEofBatch(batch5));
+}
+
 INSTANTIATE_TEST_SUITE_P(TestParam, AvroFileBatchReaderTest, ::testing::Values(false, true));
 
 }  // namespace paimon::avro::test
--- test/inte/scan_and_read_inte_test.cpp
+++ test/inte/scan_and_read_inte_test.cpp
@@ -2566,6 +2566,237 @@ TEST_P(ScanAndReadInteTest, TestCastTimestampType) {
     ASSERT_TRUE(expected->Equals(read_result)) << read_result->ToString();
 }
 
+TEST_P(ScanAndReadInteTest, TestAvroWithAppendSnapshot1) {
+    auto [file_format, enable_prefetch] = GetParam();
+    if (file_format != "avro") {
+        return;
+    }
+    std::string table_path = GetDataDir() + "/avro/append_multiple.db/append_multiple";
+
+    // scan
+    ScanContextBuilder scan_context_builder(table_path);
+    scan_context_builder.AddOption(Options::SCAN_SNAPSHOT_ID, "1");
+    ASSERT_OK_AND_ASSIGN(auto scan_context, scan_context_builder.Finish());
+    ASSERT_OK_AND_ASSIGN(auto table_scan, TableScan::Create(std::move(scan_context)));
+    ASSERT_OK_AND_ASSIGN(auto result_plan, table_scan->CreatePlan());
+    ASSERT_EQ(result_plan->SnapshotId().value(), 1);
+
+    auto splits = result_plan->Splits();
+    ASSERT_EQ(3, splits.size());
+
+    // read
+    ReadContextBuilder read_context_builder(table_path);
+    ASSERT_OK_AND_ASSIGN(std::unique_ptr<ReadContext> read_context, read_context_builder.Finish());
+    ASSERT_OK_AND_ASSIGN(auto table_read, TableRead::Create(std::move(read_context)));
+    ASSERT_OK_AND_ASSIGN(auto batch_reader, table_read->CreateReader(splits));
+    ASSERT_OK_AND_ASSIGN(auto read_result, ReadResultCollector::CollectResult(batch_reader.get()));
+
+    // check result
+    auto timezone = DateTimeUtils::GetLocalTimezoneName();
+    arrow::FieldVector fields = {
+        arrow::field("_VALUE_KIND", arrow::int8()),
+        arrow::field("f0", arrow::int8()),
+        arrow::field("f1", arrow::int16()),
+        arrow::field("f2", arrow::int32()),
+        arrow::field("f3", arrow::int64()),
+        arrow::field("f4", arrow::float32()),
+        arrow::field("f5", arrow::float64()),
+        arrow::field("f6", arrow::utf8()),
+        arrow::field("f7", arrow::binary()),
+        arrow::field("f8", arrow::date32()),
+        arrow::field("f9", arrow::decimal128(5, 2)),
+        arrow::field("f10", arrow::timestamp(arrow::TimeUnit::SECOND)),
+        arrow::field("f11", arrow::timestamp(arrow::TimeUnit::MILLI)),
+        arrow::field("f12", arrow::timestamp(arrow::TimeUnit::MICRO)),
+        arrow::field("f13", arrow::timestamp(arrow::TimeUnit::SECOND, timezone)),
+        arrow::field("f14", arrow::timestamp(arrow::TimeUnit::MILLI, timezone)),
+        arrow::field("f15", arrow::timestamp(arrow::TimeUnit::MICRO, timezone)),
+        arrow::field("f16",
+                     arrow::struct_({arrow::field("f0", arrow::map(arrow::utf8(), arrow::int32())),
+                                     arrow::field("f1", arrow::list(arrow::int32()))})),
+    };
+    auto expected = std::make_shared<arrow::ChunkedArray>(
+        arrow::ipc::internal::json::ArrayFromJSON(arrow::struct_(fields), R"([
+[0, 2, 10, 1, 100, 2.0, 2.0, "two", "bbb", 123, "123.45", "1970-01-02 00:00:00", "1970-01-02 00:00:00.000", "1970-01-02 00:00:00.000000", "1970-01-02 00:00:00", "1970-01-02 00:00:00.000", "1970-01-02 00:00:00.000000",[[["key",123]],[1,2,3]]],
+[0, 1, 10, 0, 100, 1.0, 1.0, "one", "aaa", 123, "123.45", "1970-01-01 00:00:00", "1970-01-01 00:00:00.000", "1970-01-01 00:00:00.000000", "1970-01-01 00:00:00", "1970-01-01 00:00:00.000", "1970-01-01 00:00:00.000000",[[["key",123]],[1,2,3]]],
+[0, 3, 11, 0, 100, null, 3.0, "three", "ccc", 123, "123.45", "1970-01-03 00:00:00", "1970-01-03 00:00:00.000", "1970-01-03 00:00:00.000000", "1970-01-03 00:00:00", "1970-01-03 00:00:00.000", "1970-01-03 00:00:00.000000",[[["key",123]],[1,2,3]]],
+[0, 4, 11, 0, 100, 4.0, null, "four", "ddd", 123, "123.45", "1970-01-04 00:00:00", "1970-01-04 00:00:00.000", "1970-01-04 00:00:00.000000", "1970-01-04 00:00:00", "1970-01-04 00:00:00.000", "1970-01-04 00:00:00.000000",[[["key",123]],[1,2,3]]]
+])")
+            .ValueOrDie());
+    ASSERT_TRUE(expected);
+    ASSERT_TRUE(expected->Equals(read_result)) << read_result->ToString();
+}
+
+TEST_P(ScanAndReadInteTest, TestAvroWithAppendSnapshot2) {
+    auto [file_format, enable_prefetch] = GetParam();
+    if (file_format != "avro") {
+        return;
+    }
+    std::string table_path = GetDataDir() + "/avro/append_multiple.db/append_multiple";
+
+    // scan
+    ScanContextBuilder scan_context_builder(table_path);
+    scan_context_builder.AddOption(Options::SCAN_SNAPSHOT_ID, "2");
+    ASSERT_OK_AND_ASSIGN(auto scan_context, scan_context_builder.Finish());
+    ASSERT_OK_AND_ASSIGN(auto table_scan, TableScan::Create(std::move(scan_context)));
+    ASSERT_OK_AND_ASSIGN(auto result_plan, table_scan->CreatePlan());
+    ASSERT_EQ(result_plan->SnapshotId().value(), 2);
+
+    auto splits = result_plan->Splits();
+    ASSERT_EQ(3, splits.size());
+
+    // read
+    ReadContextBuilder read_context_builder(table_path);
+    ASSERT_OK_AND_ASSIGN(std::unique_ptr<ReadContext> read_context, read_context_builder.Finish());
+    ASSERT_OK_AND_ASSIGN(auto table_read, TableRead::Create(std::move(read_context)));
+    ASSERT_OK_AND_ASSIGN(auto batch_reader, table_read->CreateReader(splits));
+    ASSERT_OK_AND_ASSIGN(auto read_result, ReadResultCollector::CollectResult(batch_reader.get()));
+
+    // check result
+    auto timezone = DateTimeUtils::GetLocalTimezoneName();
+    arrow::FieldVector fields = {
+        arrow::field("_VALUE_KIND", arrow::int8()),
+        arrow::field("f0", arrow::int8()),
+        arrow::field("f1", arrow::int16()),
+        arrow::field("f2", arrow::int32()),
+        arrow::field("f3", arrow::int64()),
+        arrow::field("f4", arrow::float32()),
+        arrow::field("f5", arrow::float64()),
+        arrow::field("f6", arrow::utf8()),
+        arrow::field("f7", arrow::binary()),
+        arrow::field("f8", arrow::date32()),
+        arrow::field("f9", arrow::decimal128(5, 2)),
+        arrow::field("f10", arrow::timestamp(arrow::TimeUnit::SECOND)),
+        arrow::field("f11", arrow::timestamp(arrow::TimeUnit::MILLI)),
+        arrow::field("f12", arrow::timestamp(arrow::TimeUnit::MICRO)),
+        arrow::field("f13", arrow::timestamp(arrow::TimeUnit::SECOND, timezone)),
+        arrow::field("f14", arrow::timestamp(arrow::TimeUnit::MILLI, timezone)),
+        arrow::field("f15", arrow::timestamp(arrow::TimeUnit::MICRO, timezone)),
+        arrow::field("f16",
+                     arrow::struct_({arrow::field("f0", arrow::map(arrow::utf8(), arrow::int32())),
+                                     arrow::field("f1", arrow::list(arrow::int32()))})),
+    };
+    auto expected = std::make_shared<arrow::ChunkedArray>(
+        arrow::ipc::internal::json::ArrayFromJSON(arrow::struct_(fields), R"([
+[0, 6, 10, 1, 100, 6.0, 4.0, "six", "fff", 123, "123.45", "1970-01-02 00:00:00", "1970-01-06 00:00:00.000", "1970-01-06 00:00:00.000000", "1970-01-06 00:00:00", "1970-01-06 00:00:00.000", "1970-01-06 00:00:00.000000",[[["key",123]],[1,2,3]]],
+[0, 5, 10, 0, 100, 5.0, 2.0, null, "eee", 123, "123.45", "1970-01-01 00:00:00", "1970-01-05 00:00:00.000", "1970-01-05 00:00:00.000000", "1970-01-05 00:00:00", "1970-01-05 00:00:00.000", "1970-01-05 00:00:00.000000",[[["key",123]],[1,2,3]]],
+[0, 7, 11, 0, 100, 7.0, 6.0, "seven", "ggg", 123, "123.45", "1970-01-03 00:00:00", "1970-01-07 00:00:00.000", "1970-01-07 00:00:00.000000", "1970-01-07 00:00:00", "1970-01-07 00:00:00.000", "1970-01-07 00:00:00.000000",[[["key",123]],[1,2,3]]]
+])")
+            .ValueOrDie());
+    ASSERT_TRUE(expected);
+    ASSERT_TRUE(expected->Equals(read_result)) << read_result->ToString();
+}
+
+TEST_P(ScanAndReadInteTest, TestAvroWithPkSnapshot1) {
+    auto [file_format, enable_prefetch] = GetParam();
+    if (file_format != "avro") {
+        return;
+    }
+    std::string table_path = GetDataDir() + "/avro/pk_with_multiple_type.db/pk_with_multiple_type";
+
+    // scan
+    ScanContextBuilder scan_context_builder(table_path);
+    scan_context_builder.AddOption(Options::SCAN_SNAPSHOT_ID, "1");
+    ASSERT_OK_AND_ASSIGN(auto scan_context, scan_context_builder.Finish());
+    ASSERT_OK_AND_ASSIGN(auto table_scan, TableScan::Create(std::move(scan_context)));
+    ASSERT_OK_AND_ASSIGN(auto result_plan, table_scan->CreatePlan());
+    ASSERT_EQ(result_plan->SnapshotId().value(), 1);
+
+    auto splits = result_plan->Splits();
+    ASSERT_EQ(1, splits.size());
+
+    // read
+    ReadContextBuilder read_context_builder(table_path);
+    ASSERT_OK_AND_ASSIGN(std::unique_ptr<ReadContext> read_context, read_context_builder.Finish());
+    ASSERT_OK_AND_ASSIGN(auto table_read, TableRead::Create(std::move(read_context)));
+    ASSERT_OK_AND_ASSIGN(auto batch_reader, table_read->CreateReader(splits));
+    ASSERT_OK_AND_ASSIGN(auto read_result, ReadResultCollector::CollectResult(batch_reader.get()));
+
+    // check result
+    arrow::FieldVector fields = {
+        arrow::field("_VALUE_KIND", arrow::int8()),
+        arrow::field("f0", arrow::boolean()),
+        arrow::field("f1", arrow::int8()),
+        arrow::field("f2", arrow::int16()),
+        arrow::field("f3", arrow::int32()),
+        arrow::field("f4", arrow::int64()),
+        arrow::field("f5", arrow::float32()),
+        arrow::field("f6", arrow::float64()),
+        arrow::field("f7", arrow::utf8()),
+        arrow::field("f8", arrow::binary()),
+        arrow::field("f9", arrow::date32()),
+        arrow::field("f10", arrow::decimal128(5, 2)),
+        arrow::field("f11",
+                     arrow::struct_({arrow::field("f0", arrow::map(arrow::utf8(), arrow::int32())),
+                                     arrow::field("f1", arrow::list(arrow::int32()))})),
+    };
+    auto expected = std::make_shared<arrow::ChunkedArray>(
+        arrow::ipc::internal::json::ArrayFromJSON(struct_(fields), R"([
+[0, false, 10, 1, 1, 1000, 1.5, 2.5, "Alice", "abcdef", 100, "123.45", [[["key",123]],[1,2,3]]],
+[0, false, 10, 1, 1, 1000, 1.5, 2.5, "Bob", "abcdef", 100, "123.45", [[["key",123]],[1,2,3]]],
+[0, true, 10, 1, 1, 1000, 1.5, 2.5, "Emily", "abcdef", 100, "123.45", [[["key",123]],[1,2,3]]],
+[0, true, 10, 1, 1, 1000, 1.5, 2.5, "Tony", "abcdef", 100, "123.45", [[["key",123]],[1,2,3]]]
+   ])")
+            .ValueOrDie());
+    ASSERT_TRUE(expected);
+    ASSERT_TRUE(expected->Equals(read_result)) << read_result->ToString();
+}
+
+TEST_P(ScanAndReadInteTest, TestAvroWithPkSnapshot2) {
+    auto [file_format, enable_prefetch] = GetParam();
+    if (file_format != "avro") {
+        return;
+    }
+    std::string table_path = GetDataDir() + "/avro/pk_with_multiple_type.db/pk_with_multiple_type";
+
+    // scan
+    ScanContextBuilder scan_context_builder(table_path);
+    scan_context_builder.AddOption(Options::SCAN_SNAPSHOT_ID, "2");
+    ASSERT_OK_AND_ASSIGN(auto scan_context, scan_context_builder.Finish());
+    ASSERT_OK_AND_ASSIGN(auto table_scan, TableScan::Create(std::move(scan_context)));
+    ASSERT_OK_AND_ASSIGN(auto result_plan, table_scan->CreatePlan());
+    ASSERT_EQ(result_plan->SnapshotId().value(), 2);
+
+    auto splits = result_plan->Splits();
+    ASSERT_EQ(1, splits.size());
+
+    // read
+    ReadContextBuilder read_context_builder(table_path);
+    ASSERT_OK_AND_ASSIGN(std::unique_ptr<ReadContext> read_context, read_context_builder.Finish());
+    ASSERT_OK_AND_ASSIGN(auto table_read, TableRead::Create(std::move(read_context)));
+    ASSERT_OK_AND_ASSIGN(auto batch_reader, table_read->CreateReader(splits));
+    ASSERT_OK_AND_ASSIGN(auto read_result, ReadResultCollector::CollectResult(batch_reader.get()));
+
+    // check result
+    arrow::FieldVector fields = {
+        arrow::field("_VALUE_KIND", arrow::int8()),
+        arrow::field("f0", arrow::boolean()),
+        arrow::field("f1", arrow::int8()),
+        arrow::field("f2", arrow::int16()),
+        arrow::field("f3", arrow::int32()),
+        arrow::field("f4", arrow::int64()),
+        arrow::field("f5", arrow::float32()),
+        arrow::field("f6", arrow::float64()),
+        arrow::field("f7", arrow::utf8()),
+        arrow::field("f8", arrow::binary()),
+        arrow::field("f9", arrow::date32()),
+        arrow::field("f10", arrow::decimal128(5, 2)),
+        arrow::field("f11",
+                     arrow::struct_({arrow::field("f0", arrow::map(arrow::utf8(), arrow::int32())),
+                                     arrow::field("f1", arrow::list(arrow::int32()))})),
+    };
+    auto expected = std::make_shared<arrow::ChunkedArray>(
+        arrow::ipc::internal::json::ArrayFromJSON(struct_(fields), R"([
+[0, false, 10, 1, 1, 1000, 1.5, 2.5, "Alice", "abcdef", 100, "123.45", [[["key",123]],[1,2,3]]],
+[0, false, 10, 1, 1, 1000, 1.5, 2.5, "Bob", "abcdef", 100, "123.45", [[["key",123]],[1,2,3]]],
+[0, true, 10, 1, 1, 1000, 1.5, 2.5, "Lucy", "abcdef", 100, "123.45", [[["key",123]],[1,2,3]]],
+[0, true, 10, 1, 1, 1000, 1.5, 2.5, "Tony", "abcdef", 100, "123.45", [[["key",123]],[1,2,3]]]
+   ])")
+            .ValueOrDie());
+    ASSERT_TRUE(expected);
+    ASSERT_TRUE(expected->Equals(read_result)) << read_result->ToString();
+}
+
 std::vector<std::pair<std::string, bool>> GetTestValuesForScanAndReadInteTest() {
     std::vector<std::pair<std::string, bool>> values = {{"parquet", false}, {"parquet", true}};
 #ifdef PAIMON_ENABLE_ORC
