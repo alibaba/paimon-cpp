8faf048017a637608378c41c2274064f4d08230f
lxy264173 <lxy264173@alibaba-inc.com>
2026-01-27 21:20:35 +0800

    fix boost

--- cmake_modules/ThirdpartyToolchain.cmake
+++ cmake_modules/ThirdpartyToolchain.cmake
@@ -409,6 +409,10 @@ macro(build_boost)
     file(MAKE_DIRECTORY ${BOOST_INCLUDE_DIR})
     file(MAKE_DIRECTORY ${BOOST_LIBRARY_DIR})
 
+    set(Boost_INCLUDE_DIR ${BOOST_INCLUDE_DIR})
+    set(Boost_LIBRARY_DIR ${BOOST_LIBRARY_DIR})
+    set(Boost_NO_SYSTEM_PATHS ON)
+    
     set(BOOST_BYPRODUCTS
         ${BOOST_LIBRARY_DIR}/libboost_date_time.a
         ${BOOST_LIBRARY_DIR}/libboost_filesystem.a
