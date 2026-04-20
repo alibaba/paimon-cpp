f52ba5b6372543d3cb0c37fd02d1427b4bd091b8
zhangchaoming.zcm <zhangchaoming.zcm@antgroup.com>
2026-04-20 21:53:21 +0800

    feat: Add merge method for DeletionVector

--- src/paimon/core/deletionvectors/bitmap_deletion_vector.cpp
+++ src/paimon/core/deletionvectors/bitmap_deletion_vector.cpp
@@ -84,4 +84,18 @@ Result<PAIMON_UNIQUE_PTR<DeletionVector>> BitmapDeletionVector::Deserialize(cons
                                          length - MAGIC_NUMBER_SIZE_BYTES, pool);
 }
 
+Status BitmapDeletionVector::Merge(const std::shared_ptr<DeletionVector>& deletionVector) {
+    if (!deletionVector || deletionVector->IsEmpty()) {
+        return Status::OK();
+    }
+    auto* other = dynamic_cast<BitmapDeletionVector*>(deletionVector.get());
+    if (other != nullptr) {
+        roaring_bitmap_ |= other->roaring_bitmap_;
+    } else {
+        return Status::Invalid(
+            "Cannot merge a non-BitmapDeletionVector into a BitmapDeletionVector");
+    }
+    return Status::OK();
+}
+
 }  // namespace paimon
