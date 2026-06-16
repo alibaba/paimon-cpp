b07c628ca5c11ca891c0d5540f613b0a1b39d752
yonghao.fyh <yonghao.fyh@alibaba-inc.com>
2026-06-16 09:11:54 +0800

    fix

--- src/paimon/core/operation/internal_read_context_test.cpp
+++ src/paimon/core/operation/internal_read_context_test.cpp
@@ -195,10 +195,9 @@ TEST(InternalReadContext, TestReadWithFieldIdsAndSpecialFields) {
 TEST(InternalReadContext, TestReadWithProjectedSchemaAndSpecialFields) {
     std::string path = paimon::test::GetDataDir() + "/orc/append_09.db/append_09";
 
-    std::vector<DataField> projected_fields = {DataField(0, arrow::field("f0", arrow::utf8())),
-                                               SpecialFields::RowId(),
-                                               SpecialFields::SequenceNumber(),
-                                               SpecialFields::IndexScore()};
+    std::vector<DataField> projected_fields = {
+        DataField(0, arrow::field("f0", arrow::utf8())), SpecialFields::RowId(),
+        SpecialFields::SequenceNumber(), SpecialFields::IndexScore()};
     auto schema_manager = SchemaManager(std::make_shared<LocalFileSystem>(), path);
     ASSERT_OK_AND_ASSIGN(auto table_schema, schema_manager.ReadSchema(0));
 
@@ -211,9 +210,9 @@ TEST(InternalReadContext, TestReadWithProjectedSchemaAndSpecialFields) {
         context_builder.SetReadSchema(&c_schema);
         ASSERT_OK_AND_ASSIGN(auto unique_read_context, context_builder.Finish());
         std::shared_ptr<ReadContext> read_context = std::move(unique_read_context);
-        ASSERT_NOK_WITH_MSG(InternalReadContext::Create(read_context, table_schema,
-                                                        table_schema->Options()),
-                            "not exist in table schema");
+        ASSERT_NOK_WITH_MSG(
+            InternalReadContext::Create(read_context, table_schema, table_schema->Options()),
+            "not exist in table schema");
     }
 
     // With options enabled, projected-schema path should accept these special fields.
@@ -229,9 +228,9 @@ TEST(InternalReadContext, TestReadWithProjectedSchemaAndSpecialFields) {
         context_builder.SetReadSchema(&c_schema);
         ASSERT_OK_AND_ASSIGN(auto unique_read_context, context_builder.Finish());
         std::shared_ptr<ReadContext> read_context = std::move(unique_read_context);
-        ASSERT_OK_AND_ASSIGN(auto internal_context,
-                             InternalReadContext::Create(read_context, table_schema,
-                                                         enabled_options));
+        ASSERT_OK_AND_ASSIGN(
+            auto internal_context,
+            InternalReadContext::Create(read_context, table_schema, enabled_options));
         auto expected_schema = DataField::ConvertDataFieldsToArrowSchema(projected_fields);
         ASSERT_TRUE(internal_context->GetReadSchema()->Equals(expected_schema));
     }
