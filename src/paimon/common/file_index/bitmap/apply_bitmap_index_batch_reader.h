fcc1ac87d4320661fba158230681990117d4a8d7
zhouhongfeng <zhouhongfeng.zhf@alibaba-inc.com>
2026-06-29 10:55:15 +0800

    fix: use iterator to apply bitmap

--- src/paimon/common/file_index/bitmap/apply_bitmap_index_batch_reader.h
+++ src/paimon/common/file_index/bitmap/apply_bitmap_index_batch_reader.h
@@ -94,12 +94,24 @@ class ApplyBitmapIndexBatchReader : public FileBatchReader {
 
  private:
     Result<RoaringBitmap32> Filter(int32_t batch_size) const {
-        RoaringBitmap32 is_valid;
+        RoaringBitmap32 result;
+        auto bitmap_iter = bitmap_.Begin();
+        auto bitmap_end = bitmap_.End();
+
         for (int32_t i = 0; i < batch_size; ++i) {
             PAIMON_ASSIGN_OR_RAISE(uint64_t file_row_id, reader_->GetPreviousBatchFileRowId(i));
-            is_valid.Add(i);
+            while (bitmap_iter != bitmap_end &&
+                   static_cast<uint64_t>(*bitmap_iter) < file_row_id) {
+                ++bitmap_iter;
+            }
+            if (bitmap_iter == bitmap_end) {
+                break; 
+            }
+            if (static_cast<uint64_t>(*bitmap_iter) == file_row_id) {
+                result.Add(i);
+            }
         }
-        return RoaringBitmap32::And(bitmap_, is_valid);
+        return result;
     }
 
  private:
